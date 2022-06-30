8c1f52a16ce11a4c47e6cda713a3a584ab77fd99
hasan <mdhasanhk.98@gmail.com>
2022-06-30 16:19:43 -0400

    shoping card

--- script.js
+++ script.js
@@ -40,33 +40,55 @@
 //     });
 
 
-function phoneSellingIncrease(isIncrease) {
-    const phoneInput = document.getElementById('phone-count');
-    const phoneCount = parseInt(phoneInput.value);
-    let phoneNewCount = phoneCount;
+
+function handleProductChange(product, isIncrease) {
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    let productNewCount = productCount;
     if(isIncrease == true) {
-        phoneNewCount = phoneCount + 1;
+        productNewCount = productCount + 1;
     }
-    if(isIncrease == false && phoneCount > 0)  {
-        phoneNewCount = phoneCount -1;
+    if(isIncrease == false && productCount > 0) {
+        productNewCount = productCount - 1;
     }
-    phoneInput.value = phoneNewCount;
-    const phoneTotal = phoneNewCount * 1219;
-    document.getElementById('phone-total').innerText = phoneTotal;
+    productInput.value = productNewCount;
+    // productTotal = productNewCount * 59;
+    let productTotal = 0;
+    if(product == 'phone') {
+        productTotal = productNewCount * 1219;
+    }
+    if(product == 'case') {
+        productTotal = productNewCount * 59;
+    }
+    document.getElementById(product +'-total').innerText = '$' + productTotal;
 }
+// function handlePhoneChange(isIncrease) {
+//     const phoneInput = document.getElementById('phone-count');
+//     const phoneCount = parseInt(phoneInput.value);
+//     let phoneNewCount = phoneCount;
+//     if(isIncrease == true) {
+//         phoneNewCount = phoneCount + 1;
+//     }
+//     if(isIncrease == false && phoneCount > 0)  {
+//         phoneNewCount = phoneCount -1;
+//     }
+//     phoneInput.value = phoneNewCount;
+//     const phoneTotal = phoneNewCount * 1219;
+//     document.getElementById('phone-total').innerText = phoneTotal;
+// }
 
 
-    function caseSellingIncrease(isIncrease) {
-        const caseInput = document.getElementById('case-count');
-        const caseCount = parseInt(caseInput.value);
-        let caseNewCount = caseCount;
-        if(isIncrease == true) {
-            caseNewCount = caseCount + 1;
-        }
-        if(isIncrease == false && caseCount > 0) {
-            caseNewCount = caseCount - 1;
-        }
-        caseInput.value = caseNewCount;
-        caseTotal = caseNewCount * 59;
-        document.getElementById('case-total').innerText = '$' + caseTotal;
-    }
+//     function handleProductChange(isIncrease) {
+//         const caseInput = document.getElementById('case-count');
+//         const caseCount = parseInt(caseInput.value);
+//         let caseNewCount = caseCount;
+//         if(isIncrease == true) {
+//             caseNewCount = caseCount + 1;
+//         }
+//         if(isIncrease == false && caseCount > 0) {
+//             caseNewCount = caseCount - 1;
+//         }
+//         caseInput.value = caseNewCount;
+//         caseTotal = caseNewCount * 59;
+//         document.getElementById('case-total').innerText = '$' + caseTotal;
+//     }
